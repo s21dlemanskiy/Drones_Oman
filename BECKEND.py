aa99a7db643a7a45991e45b0f1cfab1dc25efa79
s21dlemanskiy <55732402+s21dlemanskiy@users.noreply.github.com>
2021-10-28 12:58:49 +0300

    f

--- BECKEND.py
+++ BECKEND.py
@@ -1,3 +1,18 @@
+def read_geojson(file="./Data/regions.geojson"):
+    a = open(file, "r")
+    file = a.readline()
+    f = []
+    for i in file.split("{"):
+        f += list(i.split("}"))
+    d = False
+    for i in f:
+        print(i)
+        # if d:
+        #     print(i)
+        # if '"type":"Feature"' in i:
+        #     d = True
 
+
+read_geojson()
 
 
