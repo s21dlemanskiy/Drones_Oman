ce27887fe22d694bd2e09dbe3935e7b4d44f618e
s21dlemanskiy <55732402+s21dlemanskiy@users.noreply.github.com>
2021-10-29 10:39:26 +0300

    add Geo_jason_readeer1
    i read regions.geojason and return python date to make a deal with it

--- BECKEND.py
+++ BECKEND.py
@@ -1,18 +1,34 @@
-def read_geojson(file="./Data/regions.geojson"):
+def read_regeons_geojson(file="./Data/regions.geojson"):
     a = open(file, "r")
     file = a.readline()
     f = []
     for i in file.split("{"):
         f += list(i.split("}"))
-    d = False
+    boool = False
+    qualities = {}
+    points = {}
+    name = 'default'
     for i in f:
-        print(i)
-        # if d:
-        #     print(i)
-        # if '"type":"Feature"' in i:
-        #     d = True
+        if boool and not "geometry" in i:
+            boool = False
+            i = i[i.find("[[") + 2:i.rfind("]]")]
+            points.update({name: []})
+            while i != "":
+                points[name] += [list(map(float, i[i.find("[") + 1:i.find("]")].split(",")))]
+                i = i[i.find("]") + 1:]
+            print(i, "\n\n")
+        if "Name" in i and "marker-color" not in i:
+            name = i[i.find("Name") +   7:i[i.find("Name") + 7:].find('"') + i.find("Name") + 7]
+            qualities.update({name: {}})
+            if "population" in i:
+                qualities[name].update({"population":i[i.find("population") + 12:i[i.find("population") + 12:].find(',') + i.find("population") + 12]})
+            if "area" in i:
+                qualities[name].update({"area": i[i.find("area") + 6:i[i.find("area") + 6:].find(',') + i.find("area") + 6]})
+            boool = True
+    return points, qualities
 
 
-read_geojson()
+
+print(read_regeons_geojson())
 
 
