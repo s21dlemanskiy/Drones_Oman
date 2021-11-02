da48598f2b122177fdfcb15642dbf86f81a04a56
s21dlemanskiy <55732402+s21dlemanskiy@users.noreply.github.com>
2021-11-03 01:11:33 +0300

    upd beckend

--- BECKEND.py
+++ BECKEND.py
@@ -1,4 +1,5 @@
-import math
+import math, itertools
+delta_point = 0.0001
 class Point:
     def __init__(self, x:float, y:float):
         self.x = x
@@ -15,9 +16,42 @@ class Point:
             print(f"y = {equatin}")
         return equatin
 
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def __repr__(self):
+        return f"<<{self.x}, {self.y}>>"
+
 
 def point_right_line(point1:Point, point2:Point, u:Point) -> bool:
-    return (u.x - point1.x) * (point2.y - point1.y) - (u.y - point1.y) * (point2.x - point1.x) > 0
+    return (u.x - point1.x) * (point2.y - point1.y) > (u.y - point1.y) * (point2.x - point1.x)
+
+
+def point_in_normal_figure(pl:List[Point], u:Point) -> List[Point]:
+    if len(pl) < 3: print("errore!!!!! Beckend  line")
+    orintation_right = point_right_line(pl[0], pl[1], pl[2])
+    point1 = pl[0]
+    for point2 in pl[1:]:
+        if orintation_right != point_right_line(point1, point2, u):
+            return False
+        point1 = point2
+    if orintation_right != point_right_line(pl[-1], plt[0], u):
+        return False
+    return True
+
+
+def normal_figure_to_points(pl:List[Point]) -> List[Point]:
+    global delta_point
+    x, y = list(map(lambda a: a.x ,pl)),list(map(lambda a: a.y ,pl))
+    x,y = ([i for i in range(min(x), max(x), delta_point)], [i for i in range(min(y), max(y), delta_point)])
+    all_points = list(map(lambda x:Point(x[0], x[1]), itertools.product(x,y)))
+    del x, y
+    new_all_points = []
+    for i in all_points:
+        if point_in_normal_figure(pl, i):
+            new_all_points += [i]
+    del all_points
+    return new_all_points
 
 
 
@@ -37,9 +71,9 @@ def read_regeons_geojson(file="./Data/regions.geojson"):
             i = i[i.find("[[") + 2:i.rfind("]]")]
             points.update({name: []})
             while i != "":
-                points[name] += [list(map(float, i[i.find("[") + 1:i.find("]")].split(",")))]
+                tmp = list(map(float, i[i.find("[") + 1:i.find("]")].split(",")))
+                points[name] += [Point(float(tmp[0]), float(tmp[1]))]
                 i = i[i.find("]") + 1:]
-            print(i, "\n\n")
         if "Name" in i and "marker-color" not in i:
             name = i[i.find("Name") +   7:i[i.find("Name") + 7:].find('"') + i.find("Name") + 7]
             qualities.update({name: {}})
@@ -50,9 +84,7 @@ def read_regeons_geojson(file="./Data/regions.geojson"):
             boool = True
     return points, qualities
 
-
-if __name__ == "__main__":
-    #print(read_regeons_geojson())
+def test1():
     x_1, y_1 = map(float, input("point1").split(" "))
     a = Point(x_1, y_1)
     x_1, y_1 = map(float, input("point2").split(" "))
@@ -60,5 +92,14 @@ if __name__ == "__main__":
     x_1, y_1 = map(float, input("point3").split(" "))
     c = Point(x_1, y_1)
     print(point_right_line(a,b, c))
+    print(a.equatian(b))
+
+def test2():
+    print(read_regeons_geojson())
+
+if __name__ == "__main__":
+    #test1()
+    test2()
+
 
 
